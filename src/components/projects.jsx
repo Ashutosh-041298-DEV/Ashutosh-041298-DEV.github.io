1fde0b8e1b7554ee95f61c3b34c6764bbfe7d536
Ash40768 <ashutoshm439@gmail.com>
2022-10-28 22:22:08 +0530

    final commit

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,6 +4,7 @@ import desk from "../images/desktime.png"
 import drum from "../images/Drumkit.png"
 import envoy from "../images/envoy.png"
 import intern from "../images/interntheory.png"
+import kind from "../images/kind.png"
 export const Projects = () => {
     
   return (
@@ -41,7 +42,7 @@ export const Projects = () => {
           </button>
           <button
             onClick={() => {
-              window.open("", "_blank");
+              window.open("https://github.com/Ashutosh-041298-DEV/interntheory", "_blank");
             }}
           >
             GitHub
@@ -83,7 +84,125 @@ export const Projects = () => {
           </button>
           <button
             onClick={() => {
-              window.open("", "_blank");
+              window.open("https://github.com/Ashutosh-041298-DEV/Bobbi-brown-clone-website", "_blank");
+            }}
+          >
+            GitHub
+          </button>
+        </div>
+      </div>
+      <div className={styles.childs}>
+        <div>
+          <center>
+            <img
+              style={{ borderRadius: "5px" }}
+              src={kind}
+              alt=""
+            />
+          </center>
+        </div>
+        <div className={styles.afterImg}>
+          <h2>Kindmeal.my website</h2>
+          <ul>
+           <li>Login and Signup functionality</li>
+           <li>pagination and Sorting </li>
+           <li>users page with json-server</li>
+           <li>backend Heroku server</li>
+          </ul>
+          <div>Tech stack- React | Json-server | heroku | styled-components | chakra-ui</div>
+          <br />
+          <button
+            onClick={() => {
+              window.open(
+                "https://scenic-friends-2114.netlify.app/",
+                "_blank"
+              );
+            }}
+          >
+            Website
+          </button>
+          <button
+            onClick={() => {
+              window.open("https://github.com/Ashutosh-041298-DEV/scenic-friends-2114", "_blank");
+            }}
+          >
+            GitHub
+          </button>
+        </div>
+      </div>
+      <div className={styles.childs}>
+        <div>
+          <center>
+            <img
+              style={{ borderRadius: "5px" }}
+              src={desk}
+              alt=""
+            />
+          </center>
+        </div>
+        <div className={styles.afterImg}>
+          <h2>Desktime Website</h2>
+          <ul>
+           <li>Login and Signup functionality</li>
+           <li>navbar and Footer </li>
+           <li>Calender using Chakra ui</li>
+           <li>Navbar change on the basis of user authentication</li>
+          </ul>
+          <div>Tech stack- React JS | Firebase | Chakra-ui</div>
+          <br />
+          <button
+            onClick={() => {
+              window.open(
+                "https://cultured-bed-2030.netlify.app/",
+                "_blank"
+              );
+            }}
+          >
+            Website
+          </button>
+          <button
+            onClick={() => {
+              window.open("https://github.com/Ashutosh-041298-DEV/cultured-bed-2030", "_blank");
+            }}
+          >
+            GitHub
+          </button>
+        </div>
+      </div>
+      <div className={styles.childs}>
+        <div>
+          <center>
+            <img
+              style={{ borderRadius: "5px" }}
+              src={envoy}
+              alt=""
+            />
+          </center>
+        </div>
+        <div className={styles.afterImg}>
+          <h2>Envoy.com Website</h2>
+          <ul>
+            <li>created using React JS </li>
+            <li>Home page Navbar and Footer with authentication</li>
+            <li>Products page with sorting and filter</li>
+            <li>Login and signup</li>
+            
+          </ul>
+          <div>Tech stack- React Js| Json-Server |chakra-ui</div>
+          <br />
+          <button
+            onClick={() => {
+              window.open(
+                "https://mute-line-8050.netlify.app/",
+                "_blank"
+              );
+            }}
+          >
+            Website
+          </button>
+          <button
+            onClick={() => {
+              window.open("https://github.com/Ashutosh-041298-DEV/mute-line-8050", "_blank");
             }}
           >
             GitHub
@@ -120,7 +239,7 @@ export const Projects = () => {
           </button>
           <button
             onClick={() => {
-              window.open("", "_blank");
+              window.open("https://github.com/Ashutosh-041298-DEV/Drumkit", "_blank");
             }}
           >
             GitHub
